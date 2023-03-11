0f79cdca71e572252bc6a7e2836214e5f715e91f
prateek <javprateekdev@gmail.com>
2023-03-11 18:22:58 +0530

    first commit

---
+++ src/Card.js
@@ -0,0 +1,138 @@
+import React from 'react'
+
+
+const Card = () => {
+  return (
+    <div style={{background:"black"}}>
+      <div style={{display:"block",marginLeft:"40%"}} >
+            <div style={{height:"12vh",borderLeft:"3px solid #b5943b",letterSpacing: "1px",fontSize: "28px", marginBottom: ""}}>
+                <div style={{display:"block"}}>
+                <div style={{marginLeft:"10px",fontSize:"28px",fontFamily: 'Cursive',color:"green"}}>Brochure</div>
+                <div style={{marginLeft:"10px",fontSize:"32px",color:"#2f2f2f",fontWeight:"700",color:"white"}} className="gold">Download E-Brochure</div>
+
+                    </div>
+</div>
+</div>
+
+    <div style={{display:"flex",margin:"%",background:"black",padding:"100px"}}>
+      
+        <div class="single_price" style={{width:"25%"}}>
+  <p class="typology">
+    APARTMENTS
+  </p>
+  <div class="price_c">
+    <h3 class="price">
+      <sup>
+        ₹
+      </sup>
+      7.77 Cr*
+    </h3>
+    <small class="onw">
+      Onwards
+    </small>
+  </div>
+  <div class="content">
+    <div class="sizes">
+      <small class="title">
+        Super Area
+      </small>
+      <p>
+        4200 Sq.Ft.
+      </p>
+    </div>
+    <div class="type">
+      <small class="title">
+        Type
+      </small>
+      <p>
+        4.5 BHK Apartments 
+      </p>
+    </div>
+    <button class="call_btn btn dark_btn rounded" data-toggle="modal" data-target="#modal1">
+      Request A Call
+    </button>
+  </div>
+</div>
+<div class="single_price" style={{width:"25%"}}>
+  <p class="typology">
+    APARTMENTS
+  </p>
+  <div class="price_c">
+    <h3 class="price">
+      <sup>
+        ₹
+      </sup>
+      7.77 Cr*
+    </h3>
+    <small class="onw">
+      Onwards
+    </small>
+  </div>
+  <div class="content">
+    <div class="sizes">
+      <small class="title">
+        Super Area
+      </small>
+      <p>
+        4200 Sq.Ft.
+      </p>
+    </div>
+    <div class="type">
+      <small class="title">
+        Type
+      </small>
+      <p>
+        4.5 BHK Apartments 
+      </p>
+    </div>
+    <button class="call_btn btn dark_btn rounded" data-toggle="modal" data-target="#modal1">
+      Request A Call
+    </button>
+  </div>
+</div>
+
+<div class="single_price snipcss-ME8DR" style={{width:"25%"}}>
+  <p class="typology">
+    APARTMENTS
+  </p>
+  <div class="price_c">
+    <h3 class="price">
+      <sup>
+        ₹
+      </sup>
+      7.77 Cr*
+    </h3>
+    <small class="onw">
+      Onwards
+    </small>
+  </div>
+  <div class="content">
+    <div class="sizes">
+      <small class="title">
+        Super Area
+      </small>
+      <p>
+        4200 Sq.Ft.
+      </p>
+    </div>
+    <div class="type">
+      <small class="title">
+        Type
+      </small>
+      <p>
+        4.5 BHK Apartments 
+      </p>
+    </div>
+    <button class="call_btn btn dark_btn rounded" data-toggle="modal" data-target="#modal1">
+      Request A Call
+    </button>
+  </div>
+</div>
+
+
+    </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/FloorPlan.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FloorPlan = () => {
+  return (
+    <div style={{height:"40vh",}}>
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default FloorPlan
